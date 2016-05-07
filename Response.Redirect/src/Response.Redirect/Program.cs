eec69d26cb02ea3063748f4d24bbfc56c93a5bd5
Hussen Fakhrulddin <hf556482@gmail.com>
2016-05-06 23:44:04 -0700

    init

---
+++ Response.Redirect/src/Response.Redirect/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Response.Redirect
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ ResponseRedirection/ResponseRedirection/Redirection.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ResponseRedirection
+{
+    public static class Redirection
+    {
+        public static void Redirect(this HttpResponse response, int type, string url)
+        {
+            response.Clear();
+
+            switch (type)
+            {
+                case 301:
+                    response.StatusCode = (int)HttpStatusCode.MovedPermanently;
+                    response.StatusDescription = "Moved Permanently";
+                    break;
+
+                case 302:
+                    response.StatusCode = (int)HttpStatusCode.Found;
+                    response.StatusDescription = "Found";
+                    break;
+
+                case 303:
+                    response.StatusCode = (int)HttpStatusCode.SeeOther;
+                    response.StatusDescription = "See Other";
+                    break;
+
+                case 304:
+                    response.StatusCode = (int)HttpStatusCode.NotModified;
+                    response.StatusDescription = "Not Modified";
+                    break;
+
+                case 307:
+                    response.StatusCode = (int)HttpStatusCode.TemporaryRedirect;
+                    response.StatusDescription = "Temporary Redirect";
+                    break;
+
+                default:
+                    goto case 302;
+            }
+
+            response.RedirectLocation = url;
+
+            response.ContentType = "text/html";
+            response.Write("<html><head><title>Object Moved</title></head><body>");
+            response.Write("<h2>Object moved to <a href=\"" + HttpUtility.HtmlAttributeEncode(url) + "\">here</a>.</h2>");
+            response.Write("</body></html>");
+
+            response.End();
+        }
+    }
+}
